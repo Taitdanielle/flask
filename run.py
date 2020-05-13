1869159c8a1e4c8731e1ad4b83116064fda46476
Danielle Tait <Danielletait@hotmail.co.uk>
2020-05-13 22:20:01 +0000

    .

--- run.py
+++ run.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template("index.html")
-    
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
