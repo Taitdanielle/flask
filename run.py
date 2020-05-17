a2074aba3cef52227317657dd6ff163ea8729840
Danielle Tait <Danielletait@hotmail.co.uk>
2020-05-17 18:08:48 +0000

    added changes to index.html

--- run.py
+++ run.py
@@ -24,6 +24,6 @@ def careers():
     return render_template("careers.html")
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', '5000')),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
